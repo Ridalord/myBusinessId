204e94c80fa0cb617dae8e2c7d699430dc54718f
Ridalord <ridhwan.alabi@gmail.com>
2022-11-13 15:07:09 +0100

    updated react app

--- src/Components/userAbout.jsx
+++ src/Components/userAbout.jsx
@@ -7,9 +7,13 @@ export default function About() {
                 <h4>About</h4>
                 <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up to security and best practices, and I'm always looking for new things to learn.</p>
             </div>
+            <div className="about">
+                <h4>Skills</h4>
+                <p>HTML5, CSS, Javascript, React.js, SEO.</p>
+            </div>
             <div>
                 <h4>Interests</h4>
-                <p>Reader, Internet fanatic, IT, Cycling, Travel geek, Music, Religion</p>
+                <p>Reader, Internet fanatic, IT, Cycling, Travel geek, Music, Religion.</p>
             </div>
         </div>
     )
